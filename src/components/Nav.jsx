2c02934ef7fe65c40896d61de4f1651b22a9e323
paulina koch <pjassakoch@gmail.com>
2024-07-10 16:16:49 -0400

    Adding a nav component. Added the logo and styling for the nav, just a simple box-shadow

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+       <nav>
+        <img src="/public/airbnb-seeklogo.svg" alt="airbnb Logo" className='logo' />
+       
+        </nav>
+    );
+}
