5465eba89ada8cb59e3e82215269b0b3ef786b97
paulina koch <pjassakoch@gmail.com>
2024-07-30 13:27:56 -0400

    Fixed css issues with the .photo-grid overflowing in the full screen view and added a media query for the .photogrid.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 
 export default function Hero() {
     return (
-       <div className="hero">
-   <div className="photo-grid">
-    <div className="photo" id="photo1"> <img src="/concert.jpg" alt="concert" /></div>
-    <div className="photo" id="photo2"> <img src="/fishing.jpg" alt="fishing" /></div>
-    <div className="photo" id="photo3"> <img src="/karaoke.jpg" alt="karaoke" /></div>
-    <div className="photo" id="photo4"> <img src="/people.jpg" alt="people" /></div>
-    <div className="photo" id="photo5"> <img src="/peopleEating.jpg" alt="peopleEating" /></div>
-    <div className="photo" id="photo6"> <img src="/yoga.jpg" alt="yoga" /></div>
-    <div className="photo" id="photo7"> <img src="/yougurt.jpg" alt="yogurt" /></div>
+        <section className="hero">
+           
+                <div className="photo-grid">
+                    <div className="photo" id="photo1"> <img src="/concert.jpg" alt="concert" /></div>
+                    <div className="photo" id="photo2"> <img src="/fishing.jpg" alt="fishing" /></div>
+                    <div className="photo" id="photo3"> <img src="/karaoke.jpg" alt="karaoke" /></div>
+                    <div className="photo" id="photo4"> <img src="/people.jpg" alt="people" /></div>
+                    <div className="photo" id="photo5"> <img src="/peopleEating.jpg" alt="peopleEating" /></div>
+                    <div className="photo" id="photo6"> <img src="/yoga.jpg" alt="yoga" /></div>
+                    <div className="photo" id="photo7"> <img src="/yougurt.jpg" alt="yogurt" /></div>
+                </div>
 
- 
-   </div>
-       <div className="hero-text">
-   <h1>Online Experiences</h1>
-   <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
-   </div>
-        </div>
+         
+            <div className="hero-header">
+                <h1 className="hero-title">Online Experiences</h1>
+                <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+            </div>
+        </section>
     );
 }
